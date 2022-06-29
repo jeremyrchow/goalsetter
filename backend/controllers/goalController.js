3c42f0337ad7efc0cdca88056b0b5a69253b05e4
Jeremy <jeremyrchow@gmail.com>
2022-06-28 23:35:56 -0700

    First version of api

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,40 +1,61 @@
 
 const asyncHandler = require('express-async-handler')
+const Goal = require ('../model/goalModel')
 
 // @desc    Get goals
 // @route   GET /api/goals
 // @access  Private
 const getGoals = asyncHandler(async (req,res) => {
-  res.status(200).json({message: 'Get goals'})
+  const goals = await Goal.find()
+  res.status(200).json(goals)
 })
 
-// @desc    Get goals
-// @route   GET /api/goals
+// @desc    Set goals
+// @route   SET /api/goals
 //@access  Private
-
 const setGoals = asyncHandler(async (req,res) => {
   if(!req.body.text) {
     res.status(400)
     throw new Error('Please add a text field')
   }
-
-  res.status(200).json({message: 'post goals'})
+  const goal = await Goal.create({
+    text: req.body.text
+  })
+  res.status(200).json(goal)
 })
 
-// @desc    Get goals
-// @route   GET /api/goals
+// @desc    Update goals
+// @route   PUT /api/goals
 // @access  Private
-
 const updateGoals = asyncHandler(async (req,res) => {
-  res.status(200).json({message: `Update goals ${req.params.id}`})
+  // if(!req.params.id || mongoose.Types.ObjectId.isValid(req.params.id)){
+  //   res.status(400)
+  //   throw new Error('Goal with specified ID not found')
+  // }
+  const goal = await Goal.findById(req.params.id)
+  if(!goal){
+    res.status(400)
+    throw new Error('Goal with specified ID not found')
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body,
+     {new: true})
+
+  res.status(200).json(updatedGoal)
 })
 
-// @desc    Get goals
-// @route   GET /api/goals
+// @desc    Delete goals
+// @route   DELETE /api/goals
 // @access  Private
 
 const deleteGoals = asyncHandler(async (req,res) => {
-  res.status(200).json({message: `Delete goals ${req.params.id}`})
+  const goal = await Goal.findById(req.params.id)
+  if(!goal){
+    res.status(400)
+    throw new Error('Goal with specified ID not found')
+  }
+
+  const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
+  res.status(200).json(deletedGoal)
 })
 
 module.exports = {
