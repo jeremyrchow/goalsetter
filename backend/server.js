fb189aea7c6ee83611c1b57ce7345b81a1f8e594
Jeremy C <jchow@jeremys-mbp-2.lan>
2022-07-02 00:03:49 -0400

    Preparing for heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,15 @@ app.use(express.urlencoded({extended: false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/build')))
+
+    app.get('*',(req, res) => 
+        res.sendFile(path.resolve(__dirname, '../', 'frontend','build','index.html')))
+}
+else{
+    app.get('/', (req,res) => res.send('Set to prod.'))
+}
+
 app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
